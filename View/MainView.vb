de904902a2f6859af3fdda4d17d3b03fcfac9efe
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-09-02 20:56:16 +0900

    除去したコントロールのイベントハンドラを削除

--- View/MainView.vb
+++ View/MainView.vb
@@ -1,39 +1,12 @@
 ﻿Public Class MainView
 
-Private Sub RunCommand(ByVal command As String)
+Private Sub RunCommand()
 ''--------------------------------------------------------------------
 ''    指定したコマンドを実行する。
 ''--------------------------------------------------------------------
 
-    Using process As New System.Diagnostics.Process()
-        process.StartInfo.FileName = "ipconfig.exe"
-        process.StartInfo.UseShellExecute = False
-        process.StartInfo.RedirectStandardInput = False
-        process.StartInfo.RedirectStandardOutput = True
-        process.StartInfo.RedirectStandardError = False
-        process.Start()
-
-        Dim Reader As System.IO.StreamReader = process.StandardOutput
-        Dim output As String = Reader.ReadToEnd()
-
-        txtOutput.Text = output
-        process.WaitForExit()
-        process.Close()
-    End Using
-
 End Sub
 
-
-Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles _
-            btnRun.Click
-''--------------------------------------------------------------------
-''    「実行」ボタンのクリックイベントハンドラ。
-''
-''    入力したコマンドを実行する。
-''--------------------------------------------------------------------
-
-    RunCommand(txtCommand.Text)
-End Sub
 
 Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles _
             mnuFileExit.Click
@@ -51,7 +24,7 @@ Private Sub mnuRunCommand_Click(sender As Object, e As EventArgs) Handles _
 ''    メニュー「実行」－「コマンドを実行」
 ''--------------------------------------------------------------------
 
-    RunCommand(txtCommand.Text)
+    RunCommand()
 End Sub
 
 End Class
